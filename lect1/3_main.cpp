d46530364e77e68a517f3f368949ea47417729d1
chandankumar12a789-svg <chandankumar12a789@gmail.com>
2026-07-28 10:54:19 +0000

    2_maim.cpp saved

---
+++ lect1/3_main.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {cout << "hello" << endl << 10 << endl << 3.14 << endl << 'x' << endl; return 0;}
---
+++ lect1/5_main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int x;
+	x = 100;
+	cout << x << endl;
+
+	double y;
+	y = 3.14;
+	cout << y << endl;
+
+	int z;
+	z = 10 + 20;
+	cout << z << endl;
+
+
+	int i = 1; //variable initialisation
+
+	int j; // variable declaration
+	j = 1; // variable assignment
+
+	return 0;
+}
